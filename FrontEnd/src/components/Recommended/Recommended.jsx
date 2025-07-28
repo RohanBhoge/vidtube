47dfad9f2af7ee0c2759e8350a773d1a64239a92
Rohan <bhogerohan12@gmail.com>
2025-07-28 19:32:29 +0530

    fix: index.js file path

--- FrontEnd/src/components/Recommended/Recommended.jsx
+++ FrontEnd/src/components/Recommended/Recommended.jsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import "./Recommended.css";
 import { Link } from "react-router-dom";
 import { API_KEY, value_converter } from "../../Data";
+import { useContext } from "react";
+import VideoContext from "../../context/video/VideoContext";
 const Recommended = ({ categoryId }) => {
   const [apiData, setApiData] = useState([]);
+  const { setVideoId } = useContext(VideoContext);
+
   const fetchData = async () => {
     const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=IN&videoCategoryId=${"0"}&key=${API_KEY}`;
     await fetch(relatedVideo_url)
@@ -23,6 +27,12 @@ const Recommended = ({ categoryId }) => {
             to={`/video/${item.snippet.categoryID}/${item.id}`}
             key={index}
             className="side-video-list"
+            onClick={() => {
+              setVideoId(() => {
+                if (typeof item.id === "string") return item.id;
+                else return item.id.videoId;
+              });
+            }}
           >
             <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className="vid-info">
